51508b0401fa47f5e1f100c5301af38cd2c02ad1
technillogue <technillogue@gmail.com>
2022-05-01 02:08:42 -0400

    inventory

--- new.py
+++ new.py
@@ -1,24 +1,33 @@
 from tc import TerminalController as TermCtrl
 from random import randint, choice
+from collections import defaultdict
+import urllib
 import tty
 import sys
 
 tty.setcbreak(sys.stdin.fileno())  # cbreak mode, no buffer
 term = TermCtrl()
 
-life = 10
-points = 0
+inventory = defaultdict(lambda: 0)
+
+names = {"l": "Life", "p": "Points"}
 
 
 def blit(board, cursor):
+    inv = ", ".join(
+        "%s: %s" % (names.get(k, k), 10 - v if k == "l" else v)
+        for k, v in inventory.items()
+        if v > 2 and k.isalpha()
+    )
     sys.stdout.write(
         term.CLEAR_SCREEN
         + "\n".join("".join(row) for row in board)
-        + "\nLife: %s Points: %s" % (life, points)
+        + (("\n" + inv + term.UP) if inv else "")
         + term.UP * (len(board) - cursor[1])
         + term.BOL
         + term.RIGHT * (cursor[0])
     )
+
     sys.stdout.flush()
 
 
@@ -34,9 +43,17 @@ def set_cursor(board, cursor, char):
     board[cursor[1]][cursor[0]] = char
     return board
 
+
+def get_cursor(board, cursor):
+    return board[cursor[1]][cursor[0]]
+
+
 rows = 10
 cols = 20
 text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+# text = ""
+# while len(text) < rows * cols:
+#     text += urllib.urlopen("https://colbyolson.com/printers").read()
 board = [
     [highlight(char) for char in text[i * cols : i * cols + cols]] for i in range(rows)
 ]
@@ -45,9 +62,8 @@ for i in range(20):
     placed = (randint(0, cols - 1), randint(0, rows - 1))
     board = set_cursor(board, placed, highlight(choice("lp")))
 
-# board = [[text[i*j] for i in range(10)] for j in range(10)]
-board[5][5] = "@"
 cursor = (5, 5)
+board[5][5] = "@"
 blit(board, cursor)
 
 
@@ -77,16 +93,13 @@ while 1:
         new_x, new_y = moves[char](*cursor)
         if 0 <= new_x < cols and 0 <= new_y < rows:
             cursor = (new_x, new_y)
-    new_char = board[cursor[1]][cursor[0]].lower()
-    if "l" in new_char:
-        life -= 1
-    if "p" in new_char:
-        points += 1
+    new_char = get_cursor(board, cursor).lower()
+    inventory[new_char] += 1
+    # # move the letter to the next position in the path
+    # set_cursor(board, moves[char](*cursor), new_char)
     set_cursor(board, cursor, "@")
     blit(board, cursor)
-    if life == 0:
-        print "\n" * (len(board) - cursor[1]) + "\n\nyou loose"
-        sys.exit(0)
-    if points >= 10:
-        print "\n\nyou win"
+    state = "win" if inventory["p"] >= 10 else "lose" if inventory["l"] >= 10 else ""
+    if state:
+        sys.stdout.write("\n" * (len(board) - cursor[1]) + "\n\nyou %s" % state)
         sys.exit(0)
